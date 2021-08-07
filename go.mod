444cd67c780da4612677a709805d240290bc031f
angusbean <angus.bean.design@gmail.com>
2021-08-07 19:18:37 +1000

    all test connections working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/angusbean/test_connect
 
 go 1.15
+
+require github.com/jackc/pgx/v4 v4.13.0
--- main.go
+++ main.go
@@ -1,14 +1,118 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 func main() {
 	// connect to a database
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=dell password=chilliDog")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Unable to connect to: %v\n", err))
+	}
 
-	// test my connection
+	defer conn.Close()
 
-	// get rows from table
+	log.Println("Connected to database!")
 
-	// insert a row
+	// test the connection
+	err = conn.Ping()
+	if err != nil {
+		log.Fatal("Cannot ping database")
+	}
+
+	log.Println("Pinged database!")
+
+	// get all rows from table
+	err = getAllRows(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// insert a row using $ syntax for injection safety
+	query := `insert into users (first_name, last_name) values ($1, $2)`
+	_, err = conn.Exec(query, "Jack", "Brown")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Inserted a row!")
 
 	// get rows from table again
+	err = getAllRows(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// update a row
+	query = `update users set first_name= $1 where id = $2`
+	_, err = conn.Exec(query, "Tommy", "5")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Updated one or more rows")
+
+	// get rows from table
+	err = getAllRows(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get one row by id
+	query = `select id, first_name, last_name from users where id = $1`
+	var firstName, lastName string
+	var id int
+
+	row := conn.QueryRow(query, 1)
+	err = row.Scan(&id, &firstName, &lastName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("QueryRow returns", id, firstName, lastName)
+
+	// delete a row
+	query = `delete from users where id = $1`
+	_, err = conn.Exec(query, 6)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Deleted a row!")
+
+	// get the rows again
+	err = getAllRows(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getAllRows(conn *sql.DB) error {
+	rows, err := conn.Query("select id, first_name, last_name from users")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer rows.Close()
+
+	var firstName, lastName string
+	var id int
+
+	for rows.Next() {
+		err := rows.Scan(&id, &firstName, &lastName)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		fmt.Println("Record is", id, firstName, lastName)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal("Error scanning rows", err)
+	}
+	fmt.Println("------------------------")
+
+	return nil
 }
